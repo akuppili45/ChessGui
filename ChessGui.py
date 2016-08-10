e425cfb505623b5b17f846afcfff713c46201833
Aditya Kuppili <akuppili45@gmail.com>
2016-08-09 17:54:20 -0700

    Rename chess file

--- ChessGui.py
+++ ChessGui.py
@@ -1,89 +1,64 @@
+'''
+TODO:
+    - fix hardcoding
+    - make the board resizeable
+    - make the letters and numbers resizeable to the same scale as the board
+    - fix transparencies in gimp
+'''
 from tkinter import *
+import ctypes
 
-root = Tk()
-initialY = 95
-w = 80
-h = 80
-header = Label(text="Chess_py", anchor=CENTER, font="TimesNewRoman")
-header.pack()
+window = Tk()
+window.resizable(width=False, height=False)
 
-
-def create_alternating_color_grid(num_of_squares_per_side):
-    x = 450
-    y = 95
-    for row in range(int(num_of_squares_per_side / 2)):
-        for i in range(int(num_of_squares_per_side / 2)):
-            newcanvas = Canvas(root, width=w, height=h)
-            newcanvas.place(x=x, y=y)
-            newcanvas.create_rectangle(0, 0, w, h, fill='bisque')
-            anothercanvas = Canvas(root, width=w, height=h)
-            anothercanvas.place(x=x, y=y + h)
-            anothercanvas.create_rectangle(0, 0, w, h, fill='salmon4')
-            newcanvas = Canvas(root, width=w, height=h)
-            newcanvas.place(x=x + w, y=y)
-            newcanvas.create_rectangle(0, 0, w, h, fill='salmon4')
-            anothercanvas = Canvas(root, width=w, height=h)
-            anothercanvas.place(x=x + w, y=y + h)
-            anothercanvas.create_rectangle(0, 0, w, h, fill='bisque')
-            y += 2 * h
-        x += 2 * w
-        y = initialY
-
-
-def labelNumbers():
-    labelX = 400
-    # middle of the canvas
-    initialLabelY = 135
-    xy = 8
-    for num in range(8):
-        numLabel = Label(text=str(xy))
-        numLabel.place(x=labelX, y=initialLabelY)
-        initialLabelY += 80
-        xy -= 1
-
-
-def labelLetters():
-    letterLabelX = 490
-    letterLabelY = 755  # constant
-    lettersOfAlphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
-    for l in range(8):
-        labelList = Label(text=lettersOfAlphabet[l])
-        labelList.place(x=letterLabelX, y=letterLabelY)
-        letterLabelX += 80
-
-
-def drag_and_drop(event):
-    print()
-    '''
-    def __init__(self, num_of_squares_per_side,size):
-        self.initialY = 95
-        self.x = 450
-        self.y = 95
-
-        for row in range(int(num_of_squares_per_side / 2)):
-            for i in range(int(num_of_squares_per_side / 2)):
-                newCanvas = Canvas(root, width=size, height=size)
-                newCanvas.place(x=self.x, y=self.y)
-                box = newCanvas.create_rectangle(0, 0, size, size, fill='bisque')
-                anotherCanvas = Canvas(root, width=size, height=size)
-                anotherCanvas.place(x=self.x, y=self.y + size)
-                box1 = anotherCanvas.create_rectangle(0, 0, size, size, fill='salmon4')
-                newCanvas = Canvas(root, width=size, height=size)
-                newCanvas.place(x=self.x + size, y=self.y)
-                box2 = newCanvas.create_rectangle(0, 0, size,size, fill='salmon4')
-                anotherCanvas = Canvas(root, width=size, height=size)
-                anotherCanvas.place(x=self.x + size, y=self.y + size)
-                box3 = anotherCanvas.create_rectangle(0, 0, size, size, fill='bisque')
-                self.y += 2 * size
-            self.x += 2 * size
-            self.y = self.initialY
-            self.labelLetters()
-            self.labelNumbers()
-            newCanvas.bind("B1-Motion", self.resize_board)
+'''
+Labels and Creates the grid
 '''
 
+user32 = ctypes.windll.user32
+window.geometry(str(int(user32.GetSystemMetrics(0) / 2)) + "x" + str(user32.GetSystemMetrics(1)))
 
-create_alternating_color_grid(8)
-labelNumbers()
-labelLetters()
-root.mainloop()
+
+class Chessboard:
+    screen_height = window.winfo_screenheight()
+
+    xBoardLocation = 30
+    yBoardLocation = 30
+    canvasArray = [[Canvas for i in range(8)] for j in range(8)]
+    '''
+    Creates a canvas of a certain length and with.
+    '''
+
+    def __init__(self, num_squares, box_length):
+        # resize = Resize()
+        for row in range(int(num_squares)):
+            for col in range(int(num_squares)):
+                block = Canvas(window, width=box_length, height=box_length)
+                block.place(x=self.xBoardLocation + row * box_length, y=self.yBoardLocation + col * box_length)
+                if (col + row) % 2 == 0:
+                    block.create_rectangle(0, 0, box_length, box_length, fill='bisque')
+                else:
+                    block.create_rectangle(0, 0, box_length, box_length, fill='salmon4')
+                self.canvasArray[row][col] = block
+            self.label_board_letters(num_squares, box_length)
+            self.label_board_num(num_squares, box_length)
+
+    def label_board_num(self, num_squares, box_length):
+        x_pos = self.xBoardLocation - box_length / 4
+        y_pos = self.yBoardLocation + box_length / 4  # constant
+        for pos in range(num_squares):
+            Label(text=(num_squares - pos)).place(x=x_pos, y=y_pos)
+            y_pos += box_length
+
+    def label_board_letters(self, num_squares, box_length):
+        board_constant = 5 / 4
+        x_pos = self.xBoardLocation + box_length / 4
+        y_pos = self.yBoardLocation + (num_squares - 1 + board_constant) * box_length  # constant
+        letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+        for pos in range(num_squares):
+            Label(text=letters[pos]).place(x=x_pos, y=y_pos)
+            x_pos += box_length
+
+main_board = Chessboard(8, 80)
+
+window.mainloop()
